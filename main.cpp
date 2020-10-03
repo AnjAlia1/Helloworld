231cb533102c9ebc497797141b3107338556e954
AnjAlia1 <72298062+AnjAlia1@users.noreply.github.com>
2020-10-03 14:40:30 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,i;
+    cin>>a>>b;
+    if(a==0)
+    {
+        cout<<b;
+    }
+    if(b==0)
+    {
+        cout<<a;
+    }
+    for(i=min(a,b);i>=1;i--)
+    {
+        if((a%i==0) && (b%i==0))
+        {
+            cout<<i;
+        }
+    }
+    return 0;
+   
+}
